6ea77be6b77002604780d58816b8b12553c10892
Stephanie Zeng <stephanie.zeng@rangle.io>
2019-12-13 16:37:46 -0500

    Add password field to create user

---
+++ src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
@@ -0,0 +1,7 @@
+package com.example.demo.security;
+
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+}
