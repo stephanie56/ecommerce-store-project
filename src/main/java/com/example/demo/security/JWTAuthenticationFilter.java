b2cdb284c5d9f5bc6572f9850335a8f3031062bc
Stephanie Zeng <stephanie.zeng@rangle.io>
2019-12-13 17:05:51 -0500

    Add JWTAuthenticationFilter to generate JWT token for user

--- src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
@@ -1,7 +1,63 @@
 package com.example.demo.security;
 
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.demo.model.persistence.User;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static com.example.demo.security.SecurityConstants.*;
+
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    /** We parse the user's credentials and issue them to the AuthenticationManager. */
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) throws AuthenticationException {
+
+        try {
+            /** Convert the request payload (JSON String) to a Java object User */
+            User credential = new ObjectMapper().readValue(req.getInputStream(), User.class);
+            UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(
+                    credential.getUsername(),
+                    credential.getPassword(),
+                    new ArrayList<>()
+            );
+            return authenticationManager.authenticate(authReq);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**  The method called when a user successfully logs in. We use this method to generate a JWT for this user. */
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain, Authentication auth) throws IOException, ServletException {
+        /** Create a JWT token */
+        String token = JWT.create()
+                .withSubject(((User)auth.getPrincipal()).getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(SECRET.getBytes()));
+
+        res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+    }
 }
